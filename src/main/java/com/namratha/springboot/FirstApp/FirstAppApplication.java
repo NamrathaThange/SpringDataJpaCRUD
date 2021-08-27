9e09ba0a1e585e250d36bdf1f3863cc8c83c0ad2
NamrathaThange <namratha.thange@gmail.com>
2021-08-27 14:05:11 +0530

    Adding Swagger

--- src/main/java/com/namratha/springboot/FirstApp/FirstAppApplication.java
+++ src/main/java/com/namratha/springboot/FirstApp/FirstAppApplication.java
@@ -3,8 +3,12 @@ package com.namratha.springboot.FirstApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title="FirstApp Api", version = "1.0", description = "Provides FirstApp Services"))
 public class FirstAppApplication {
 
 	public static void main(String[] args) {
